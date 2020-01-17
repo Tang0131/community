c41359ed72ecd4eac4c50aa680f44232ecdc4f55
Tang0131 <1259169695@qq.com>
2020-01-17 23:32:12 +0800

    今天的提交

--- src/main/java/life/majiang/community/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -1,5 +1,6 @@
 package life.majiang.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
 import okhttp3.*;
@@ -32,8 +33,18 @@ public class GithubProvider {
     }
 
     public GithubUser getUser(String accessToken) {
-        OkHttpClient okHttpClient = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            JSON.parseObject(string, GithubUser.class);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new GithubUser();
     }
 
