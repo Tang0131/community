734613d818e7f565cffbb61b03b513392575390f
Tang0131 <1259169695@qq.com>
2020-01-18 02:48:28 +0800

    secend commit

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -10,13 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
     @Value("${github.client.id}")
-    private  String clientId;
+    private String clientId;
     @Value("${github.client.secret}")
     private String clientSecret;
     @Value("${github.redirect.uri}")
@@ -25,7 +27,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String State) {
+                           @RequestParam(name = "state") String State,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setRedirect_url(redirectUri);
@@ -34,12 +37,24 @@ public class AuthorizeController {
         accessTokenDTO.setState(State);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
+        if (user != null) {
+            //登录成功
+            request.getSession().setAttribute("user",user);
+            return "redirect:index";
+        } else {
+            //登录失败，重新登录
+
+        }
+
 
         return "index";
     }
 
+    public static  int  add (int a  ,int b){
 
 
+
+        return  1  ;
+    }
 
 }
